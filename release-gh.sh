0f1037790412d50b064ca119274625a70da33d29
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-06-15 02:38:26 +0300

    feat: setup release-please for automation
    - Added release-please config
    - Added release script
    - Added changelog file

---
+++ release-gh.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# ---  Main script logic --- #
+choice=$1
+
+if [[ "${choice}" == "pr" ]]; then
+  release-please release-pr \
+    --repo-url="https://github.com/AhmedOsman101/lib-result.git" \
+    --token="$GITHUB_TOKEN" \
+    --config-file="release-please-config.json"
+elif [[ "${choice}" == "gh" ]]; then
+  release-please github-release \
+    --repo-url="https://github.com/AhmedOsman101/lib-result.git" \
+    --token="$GITHUB_TOKEN" \
+    --config-file="release-please-config.json"
+else
+  release-please --help
+fi
