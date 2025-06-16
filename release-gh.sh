154646caf7f09fc3b43a66abe03fd9574ae03159
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-06-16 13:55:42 +0300

    chore: pass args to release-please commands

--- release-gh.sh
+++ release-gh.sh
@@ -4,17 +4,20 @@ set -euo pipefail
 
 # ---  Main script logic --- #
 choice=$1
+shift
 
 if [[ "${choice}" == "pr" ]]; then
   release-please release-pr \
     --repo-url="https://github.com/AhmedOsman101/lib-result.git" \
     --token="$GITHUB_TOKEN" \
-    --config-file="release-please-config.json"
+    --config-file="release-please-config.json" \
+    "$@"
 elif [[ "${choice}" == "gh" ]]; then
   release-please github-release \
     --repo-url="https://github.com/AhmedOsman101/lib-result.git" \
     --token="$GITHUB_TOKEN" \
-    --config-file="release-please-config.json"
+    --config-file="release-please-config.json" \
+    "$@"
 else
   release-please --help
 fi
