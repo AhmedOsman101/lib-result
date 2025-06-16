60942858bf32be7c8445d2984381a9f92edac34a
Ahmad Othman <ahmad.ali.othman@outlook.com>
2025-06-16 13:41:27 +0300

    build: use tsup for building the library
    - simplifies the build process
    - uses tsup for faster builds

--- build-tests.sh
+++ build-tests.sh
@@ -7,6 +7,4 @@ trap 'exit 1' SIGUSR1
 # ---  Main script logic --- #
 for file in tests/*.test.ts; do
   pnpm exec esbuild "$file" --outfile="${file%.ts}.js" --format=esm
-  output=$(sed -e 's|../src/index.js|../dist/esm/index.js|' "${file%.ts}.js")
-  echo "${output}" >"${file%.ts}.js"
 done
--- build.sh
+++ build.sh
@@ -5,17 +5,8 @@ set -euo pipefail
 trap 'exit 1' SIGUSR1
 
 # ---  Main script logic --- #
-pnpm clean && pnpm build:tests
-
-pnpm exec rollup --config
-
-mkdir -p dist/{esm,cjs}
-
-mv dist/tmp/index.esm.js dist/esm/index.js
-mv dist/tmp/index.cjs dist/cjs/index.js
-rm -rf dist/tmp
-
-cjsPkg="dist/cjs/package.json"
-
-cp package.json "${cjsPkg}"
-sed 's|"module"|"commonjs"|' package.json >"${cjsPkg}"
+pnpm clean &&
+  pnpm build:tests &&
+  pnpm format &&
+  pnpm exec tsup &&
+  pnpm exec attw --pack .
